0398c29b153ec2bed97208d3b223f6145e4acb24
ivar <ivar@ugr.es>
2025-06-02 15:28:03 +0200

    added correct answer counter as data quality filter

--- learning2/rules.js
+++ learning2/rules.js
@@ -134,12 +134,15 @@ var startpage_restaurant = {
 var test_stimuli_shoes = [
   {
     stimulus: `To keep her house clean, Mary announced: "No one may wear shoes in the house"<br><br><br><b>A guest wears stilettos and the carpets get pretty dirty.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To keep her house clean, Mary announced: "No one may wear shoes in the house"<br><br><br><b>A guest wears wet deck shoes and dirties the floors.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To keep her house clean, Mary announced: "No one may wear shoes in the house"<br><br><br><b>A guest wears muddy sneakers and dirties the carpets.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To keep her house clean, Mary announced: "No one may wear shoes in the house"<br><br><br><b>A guest wears brand new shoes and keeps the floor clean.</b><br>`,
@@ -161,12 +164,15 @@ var test_stimuli_shoes = [
   },
   {
     stimulus: `To keep her house clean, Mary announced: "No one may wear shoes in the house"<br><br><br><b>A guest walks around barefoot and keeps the carpet clean.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To keep her house clean, Mary announced: "No one may wear shoes in the house"<br><br><br><b>A guest walks around in socks and keeps the floor clean.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To keep her house clean, Mary announced: "No one may wear shoes in the house"<br><br><br><b>A guest walks around barefoot and the floor stays clean.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
 ];
 
@@ -192,21 +198,27 @@ var test_stimuli_cars = [
   },
   {
     stimulus: `To avoid accidents, the local authority issued a rule: "No cars are allowed inside the park""<br><br><br><b>A person jogs through the park to get some daily exercise.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To avoid accidents, the local authority issued a rule: "No cars are allowed inside the park""<br><br><br><b>A couple walk their new baby through the park in a stroller.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To avoid accidents, the local authority issued a rule: "No cars are allowed inside the park""<br><br><br><b>A landscaper pushes a grey wheelbarrow through the park.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To avoid accidents, the local authority issued a rule: "No cars are allowed inside the park""<br><br><br><b>A person test-drives their new car through the park at noon.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To avoid accidents, the local authority issued a rule: "No cars are allowed inside the park""<br><br><br><b>A taxi driver takes a shortcut through the park to save time.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To avoid accidents, the local authority issued a rule: "No cars are allowed inside the park""<br><br><br><b>A red party limo drives through the park at the DJ's request.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
 ];
 
@@ -217,27 +229,34 @@ var test_stimuli_trainstation = [
   },
   {
     stimulus: `To avoid people spending the night at the train station, the manager posted a rule:<br> "No sleeping allowed on the benches"<br><br><br><b>A vagrant spends the night sleeping on the station floor.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To avoid people spending the night at the train station, the manager posted a rule:<br> "No sleeping allowed on the benches"<br><br><br><b>A backpacker spends the night sleeping behind a station bench.</b><br>`,
   },
   {
     stimulus: `To avoid people spending the night at the train station, the manager posted a rule:<br> "No sleeping allowed on the benches"<br><br><br><b>A businesswoman enters the station and buys her train ticket.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To avoid people spending the night at the train station, the manager posted a rule:<br> "No sleeping allowed on the benches"<br><br><br><b>A commuter sits on a station bench while waiting for her train.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To avoid people spending the night at the train station, the manager posted a rule:<br> "No sleeping allowed on the benches"<br><br><br><b>A backpacker looks for her lost phone on the station platform.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To avoid people spending the night at the train station, the manager posted a rule:<br> "No sleeping allowed on the benches"<br><br><br><b>A vagrant spends the night sleeping on a bench at the station.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To avoid people spending the night at the train station, the manager posted a rule:<br> "No sleeping allowed on the benches"<br><br><br><b>A backpacker spends the night sleeping on a station bench.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To avoid people spending the night at the train station, the manager posted a rule:<br> "No sleeping allowed on the benches"<br><br><br><b>A nightclub dancer falls asleep on a station bench after work.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To avoid people spending the night at the train station, the manager posted a rule:<br> "No sleeping allowed on the benches"<br><br><br><b>A commuter briefly falls asleep on a bench waiting for his train.</b><br>`,
@@ -254,21 +273,27 @@ var test_stimuli_trainstation = [
 var test_stimuli_phones = [
   {
     stimulus: `To get students to pay attention in class, the headmaster announced:<br> "Phones may not be used in the classroom"<br><br><br><b>A student consults the textbook to answer a question in class.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To get students to pay attention in class, the headmaster announced:<br> "Phones may not be used in the classroom"<br><br><br><b>A student takes notes by hand to help her understand a lesson.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To get students to pay attention in class, the headmaster announced:<br> "Phones may not be used in the classroom"<br><br><br><b>A student arrives early to class and takes out her pencil case.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To get students to pay attention in class, the headmaster announced:<br> "Phones may not be used in the classroom"<br><br><br><b>A student texts friends on her phone and pays no attention.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To get students to pay attention in class, the headmaster announced:<br> "Phones may not be used in the classroom"<br><br><br><b>A student plays a game on his phone instead of paying attention.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To get students to pay attention in class, the headmaster announced:<br> "Phones may not be used in the classroom"<br><br><br><b>A student browses her phone instead of following her lesson.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To get students to pay attention in class, the headmaster announced:<br> "Phones may not be used in the classroom"<br><br><br><b>A student uses his phone's calculator for the in-class assignment.</b><br>`,
@@ -303,21 +328,27 @@ var test_stimuli_deer = [
   },
   {
     stimulus: `To protect the deer population, parliament enacts a law: "It is an offense to shoot deer"<br><br><br><b>A man heads deep into the local forest and sees several deer.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To protect the deer population, parliament enacts a law: "It is an offense to shoot deer"<br><br><br><b>A woman hikes in the woods carrying a flashlight and a lunch box.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To protect the deer population, parliament enacts a law: "It is an offense to shoot deer"<br><br><br><b>A man takes a walk in the park and photographs the deer there.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To protect the deer population, parliament enacts a law: "It is an offense to shoot deer"<br><br><br><b>A man heads deep into the local forest and shoots a deer.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To protect the deer population, parliament enacts a law: "It is an offense to shoot deer"<br><br><br><b>A woman enters a wildlife reserve where she shoots two deer.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To protect the deer population, parliament enacts a law: "It is an offense to shoot deer"<br><br><br><b>A hunter, armed with a rifle, fires on the first deer he sees.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To protect the deer population, parliament enacts a law: "It is an offense to shoot deer"<br><br><br><b>A farmer shoots a deer to end its suffering after it was run over.</b><br>`,
@@ -334,21 +365,27 @@ var test_stimuli_deer = [
 var test_stimuli_lab = [
   {
     stimulus: `To avoid accidents, the director of a scientific laboratory issued a rule:<br> "Access for laboratory personnel only"<br><br><br><b>A scientist working at the laboratory starts a new experiment.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
-    stimulus: `To avoid accidents, the director of a scientific laboratory issued a rule:<br> "Access for laboratory personnel only"<br><br><br><b>A member of the public walks past the laboratory while going home.</b><br>`,
+    stimulus: `To avoid accidents, the director of a scientific laboratory issued a rule:<br> "Access for laboratory personnel only"<br><br><br><b>A member of the public stands outside the laboratory without entering.</b><br>`, // problem item1 walks past the laboratory while going home
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
-    stimulus: `To avoid accidents, the director of a scientific laboratory issued a rule:<br> "Access for laboratory personnel only"<br><br><br><b>A tourist walks past the laboratory on his way to the wax museum.</b><br>`,
+    stimulus: `To avoid accidents, the director of a scientific laboratory issued a rule:<br> "Access for laboratory personnel only"<br><br><br><b>A tourist admires the laboratory from across the street.</b><br>`, // problem item1:  walks past the laboratory on his way to the wax museum
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To avoid accidents, the director of a scientific laboratory issued a rule:<br> "Access for laboratory personnel only"<br><br><br><b>An amateur scientist enters the laboratory to take photographs.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To avoid accidents, the director of a scientific laboratory issued a rule:<br> "Access for laboratory personnel only"<br><br><br><b>A thief enters the laboratory overnight to find and steal equipment.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To avoid accidents, the director of a scientific laboratory issued a rule:<br> "Access for laboratory personnel only"<br><br><br><b>A student enters the laboratory to observe the scientists' work.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To avoid accidents, the director of a scientific laboratory issued a rule:<br> "Access for laboratory personnel only"<br><br><br><b>A skilled technician enters the lab to discuss a job opening.</b><br>`,
@@ -374,21 +411,27 @@ var test_stimuli_lab = [
 var test_stimuli_alcohol = [
   {
     stimulus: `To reduce traffic accidents, a zero tolerance law is passed:<br> "Driving with any trace of alcohol is strictly forbidden"<br><br><br><b>A sober man rides his motorcycle home after playing board games.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To reduce traffic accidents, a zero tolerance law is passed:<br> "Driving with any trace of alcohol is strictly forbidden"<br><br><br><b>A woman drives home after drinking soda at the movie theater.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To reduce traffic accidents, a zero tolerance law is passed:<br> "Driving with any trace of alcohol is strictly forbidden"<br><br><br><b>A sober man re-parks his car in the cul-de-sac to let a guest out.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `To reduce traffic accidents, a zero tolerance law is passed:<br> "Driving with any trace of alcohol is strictly forbidden"<br><br><br><b>A young man drives home after drinking heavily at a music festival.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To reduce traffic accidents, a zero tolerance law is passed:<br> "Driving with any trace of alcohol is strictly forbidden"<br><br><br><b>One night two drunk motorbikers race each other on a dark backroad.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To reduce traffic accidents, a zero tolerance law is passed:<br> "Driving with any trace of alcohol is strictly forbidden"<br><br><br><b>A woman drives home after drinking whiskey at the movie theater.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `To reduce traffic accidents, a zero tolerance law is passed:<br> "Driving with any trace of alcohol is strictly forbidden"<br><br><br><b>A neighbor sips on wine and re-parks his car in the cul-de-sac.</b><br>`,
@@ -415,14 +458,17 @@ var test_stimuli_restaurant = [
   {
     stimulus: `A  restaurant's customers had their meals interrupted by the misbehaviour of a diner's pet dog.<br>
                                To address these concerns, the owner posted a rule: "No dogs allowed in the restaurant"<br><br><br><b>A customer brings her pet bulldog into the restaurant for lunch.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `A  restaurant's customers had their meals interrupted by the misbehaviour of a diner's pet dog.<br>
                                To address these concerns, the owner posted a rule: "No dogs allowed in the restaurant"<br><br><br><b>A couple bring their pet foxhound into the restaurant for dinner.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `A  restaurant's customers had their meals interrupted by the misbehaviour of a diner's pet dog.<br>
                                To address these concerns, the owner posted a rule: "No dogs allowed in the restaurant"<br><br><br><b>A customer walks into the restaurant with his pet greyhound.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "Yes" },
   },
   {
     stimulus: `A  restaurant's customers had their meals interrupted by the misbehaviour of a diner's pet dog.<br>
@@ -451,13 +497,16 @@ var test_stimuli_restaurant = [
   {
     stimulus: `A  restaurant's customers had their meals interrupted by the misbehaviour of a diner's pet dog.<br>
                                To address these concerns, the owner posted a rule: "No dogs allowed in the restaurant"<br><br><br><b>Three colleagues have a meeting over dinner at the restaurant.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `A  restaurant's customers had their meals interrupted by the misbehaviour of a diner's pet dog.<br>
                                To address these concerns, the owner posted a rule: "No dogs allowed in the restaurant"<br><br><br><b>A couple have breakfast together at the restaurant before work.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
   {
     stimulus: `A  restaurant's customers had their meals interrupted by the misbehaviour of a diner's pet dog.<br>
                                To address these concerns, the owner posted a rule: "No dogs allowed in the restaurant"<br><br><br><b>A customer uses the restaurant WC while waiting for her food.</b><br>`,
+    data: { stim_type: "congruent", correct_response: "No" },
   },
 ];
